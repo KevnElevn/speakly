cd1d0eae1b2ecf1b11203d76e54a12fb90c06352
KevnElevn <kevinli5370@live.com>
2019-10-20 18:11:01 -0400

    cleaned up, added graphics

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import React, { useState, useEffect } from 'react';
 import uuidv4 from 'uuid/v4'
 import './App.css';
 import io from 'socket.io-client';
-const socket = io('http://10.38.128.217:3000');
+const socket = io('localhost:3000');
 var translate = require('yandex-translate')('trnsl.1.1.20191019T212733Z.c56b015e56537da4.a39c9ad7fca7503c68b03a2078d249191dc3ba22');
 
-let language = 'es'
+let language = 'en'
 
 function App() {
   console.log('re-render')
   const [msg, changeMsg] = useState('')
-  const [msgList, changeMsgList] = useState([{id: '32432', msg: 'name'}])
+  const [msgList, changeMsgList] = useState([])
   console.log(msgList)
-    
+  
   useEffect(()=> {
       socket.on('connect', function(){
         console.log('we have connected')
@@ -26,13 +26,11 @@ function App() {
           translate.translate(msg, { to: language }, (err, res) => {
             if(err) {
               // return msg;
-      
             } else {
               console.log({res})
               console.log(res.text[0])
               msg = msg + `(${res.text[0]})`;
               changeMsgList(msgList => [...msgList, {id: uuidv4(), msg}])
-      
             }
             // console.log({msg})
           });
@@ -54,9 +52,9 @@ function App() {
       <div id='chat'>
       {
         msgList.map(msgObj => {
-          return <div key={msgObj.id}>{msgObj.msg}</div>
+          return <div className='msg' key={msgObj.id}>{msgObj.msg}</div>
         })
-      } 
+      }
       <input id='input' onChange={e => changeMsg(e.target.value)} value={msg} onKeyDown={e => sendMessage(e, msg)}/>
       </div>
     </div>
