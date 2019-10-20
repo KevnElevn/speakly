a62936865614884ce56b7d40b85fd8cef793a90c
thegodofwar9732 <muhtasimmc@gmail.com>
2019-10-20 09:17:37 -0400

    fixed bug

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import React, { useState, useEffect } from 'react';
+import uuidv4 from 'uuid/v4'
 import './App.css';
 import io from 'socket.io-client';
 const socket = io('http://10.38.128.217:3000');
+var translate = require('yandex-translate')('trnsl.1.1.20191019T212733Z.c56b015e56537da4.a39c9ad7fca7503c68b03a2078d249191dc3ba22');
+
+let language = 'es'
 
 function App() {
+  console.log('re-render')
   const [msg, changeMsg] = useState('')
-
-
-    useEffect(()=> {
+  const [msgList, changeMsgList] = useState([{id: '32432', msg: 'name'}])
+  console.log(msgList)
+    
+  useEffect(()=> {
       socket.on('connect', function(){
         console.log('we have connected')
-        socket.on('newMessage', function(msg) {})
+        socket.on('newMessage', function(msg) {
+
+          console.log('GOT MESSAGE FROM BACKEND')
+          console.log("msg: " + msg)
+          console.log("msgList1: " + msgList)
+          // debugger
+          changeMsg('')
+          translate.translate(msg, { to: language }, (err, res) => {
+            if(err) {
+              // return msg;
+      
+            } else {
+              console.log({res})
+              console.log(res.text[0])
+              msg = msg + `(${res.text[0]})`;
+              changeMsgList(msgList => [...msgList, {id: uuidv4(), msg}])
+      
+            }
+            // console.log({msg})
+          });
+
+          // console.log({vari})
+        })
       });
-})
+}, [])
 
   function sendMessage({keyCode}, msg) {
     if (keyCode === 13) {
@@ -24,6 +52,11 @@ function App() {
       <div id='sidebar'>
       </div>
       <div id='chat'>
+      {
+        msgList.map(msgObj => {
+          return <div key={msgObj.id}>{msgObj.msg}</div>
+        })
+      } 
       <input id='input' onChange={e => changeMsg(e.target.value)} value={msg} onKeyDown={e => sendMessage(e, msg)}/>
       </div>
     </div>
